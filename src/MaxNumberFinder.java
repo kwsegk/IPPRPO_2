31529ee04212c4601b528d92ca470ff7a81f7dbe
Egor Kuznetsov <kuzinkuza2005@mail.ru>
2025-10-04 22:20:51 +0300

    Добавлен метод findMax

--- src/MaxNumberFinder.java
+++ src/MaxNumberFinder.java
@@ -2,4 +2,9 @@ public class MaxNumberFinder {
     public static void main(String[] args) {
         System.out.println("Программа для поиска максимума из трёх чисел");
     }
+    public static int findMax(int a, int b, int c) {
+        if (a >= b && a >= c) return a;
+        if (b >= a && b >= c) return b;
+        return c;
+    }
 }
