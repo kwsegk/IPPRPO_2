dfb68dc7bea16f4295f4399629adfc95c67e351c
Egor Kuznetsov <kuzinkuza2005@mail.ru>
2025-10-04 22:57:23 +0300

    Изменён вывод в main

--- src/MaxNumberFinder.java
+++ src/MaxNumberFinder.java
@@ -1,5 +1,6 @@
 public class MaxNumberFinder {
     public static void main(String[] args) {
         System.out.println("Программа для поиска максимума из трёх чисел");
+        System.out.println("Наибольшее число: ");
     }
 }
